d7a9508d870ba88ef691700c2de646363b3ef078
Manuj Tiwari <manujtiwari@pop-os.localdomain>
2021-07-08 12:42:28 +0530

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from './components/Navbar/Navbar';
+
 
 function App() {
   return (
     <div>
+      <Navbar/>
       <h2>This is Manuj's Portfolio ....</h2>
     </div>
   );
